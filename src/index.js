56ffc8349ec0b7d9e6e324b9425872b1f8913ef8
YY <z3388638@gmail.com>
2017-11-18 01:53:47 +0800

    init firebase

--- src/index.js
+++ src/index.js
@@ -6,5 +6,16 @@ import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
+// Initialize Firebase
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: '',
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+window.firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('app'));
 registerServiceWorker();
