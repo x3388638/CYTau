83e1eee900b8b873814dcfd8e78e2f4cdd75b649
YY <z3388638@gmail.com>
2017-11-18 13:34:36 +0800

    fix font-family

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navigation from './Navigation.jsx';
 import MainContainer from './MainContainer.jsx';
 import Footer from './Footer.jsx';
 
+import './App.css';
+
 export default class App extends React.Component {
 	render () {
 		return (
